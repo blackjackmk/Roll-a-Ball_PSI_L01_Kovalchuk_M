3fe8d7d8a8b043d2c37550f83110c06bbe587d87
blackjackmk <kavalchukmaksum@gmail.com>
2024-11-15 08:43:03 +0100

    EndGameMenu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,6 +7,8 @@ public class MenuController : MonoBehaviour
 
     public GameObject OptionsPanel;
     public GameObject MainPanel;
+
+    public GameObject EndGamePanel;
     public void StartGame(){
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
@@ -18,5 +20,9 @@ public class MenuController : MonoBehaviour
     public void SettingSwitcher(bool isActive){
         OptionsPanel.SetActive(!isActive);
         MainPanel.SetActive(isActive);
+    }
+
+    public void ExitToMenu(){
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
