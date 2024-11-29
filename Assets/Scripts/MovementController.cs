9a8d8e44bcae186ff2533e32d30e9df4a45202f2
blackjackmk <kavalchukmaksum@gmail.com>
2024-11-29 11:07:49 +0100

    PostProcessing Prefab

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,6 +7,9 @@ using UnityEngine.UIElements;
 public class MovementController : MonoBehaviour
 {
     public Rigidbody rb;
+    private Material material;
+    public Material rock_material;
+    public Material volley_material;
     public float thrust = 5f;
 
     private Vector3 moveDirection;
@@ -18,6 +21,7 @@ public class MovementController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        material = GetComponent<Renderer>().material;
     }
 
     void Update(){
@@ -35,6 +39,19 @@ public class MovementController : MonoBehaviour
         }
         if(Input.GetKey(KeyCode.S)){
             moveDirection += new Vector3(1,0,0);
+        }
+        if(Input.GetKey(KeyCode.Alpha1)){//default
+            rb.mass = 5.0f;
+            GetComponent<MeshRenderer> ().material = material;
+        }
+        if(Input.GetKey(KeyCode.Alpha2)){//rock
+            rb.mass = 30.0f;
+            GetComponent<MeshRenderer> ().material = rock_material;
+            
+        }
+        if(Input.GetKey(KeyCode.Alpha3)){//volley
+            rb.mass = 1.0f;
+            GetComponent<MeshRenderer> ().material = volley_material;
         }
 
         moveDirection.Normalize();
