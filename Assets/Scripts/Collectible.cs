b8af6facead800f3f6c13336aeea3c1e69a8c77e
blackjackmk <kavalchukmaksum@gmail.com>
2024-12-13 10:20:04 +0100

    Particle on collect

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     private AudioSource audioSource;
+    private ParticleSystem particle;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        particle = GetComponent<ParticleSystem>();
     }
 
     void OnTriggerEnter(Collider collider){
         collider.gameObject.GetComponent<MovementController>().AddPoint();
+        particle.Play();
         audioSource.Play();
         gameObject.GetComponent<MeshRenderer>().enabled = false;
         gameObject.GetComponent<BoxCollider>().enabled = false;
